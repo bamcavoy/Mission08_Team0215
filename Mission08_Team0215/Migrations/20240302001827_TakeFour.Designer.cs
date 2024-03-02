2516af341170691b26faf1a38fc97a537df3f15b
Hannahkj12 <hannahkj2001@gmail.com>
2024-03-01 17:20:23 -0700

    Final

--- Mission08_Team0215/Migrations/20240302001827_TakeFour.Designer.cs
+++ Mission08_Team0215/Migrations/20240302001827_TakeFour.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mission08_Team0215.Models;
 
@@ -10,9 +11,11 @@ using Mission08_Team0215.Models;
 namespace Mission08_Team0215.Migrations
 {
     [DbContext(typeof(UserTaskFormContext))]
-    partial class UserTaskFormContextModelSnapshot : ModelSnapshot
+    [Migration("20240302001827_TakeFour")]
+    partial class TakeFour
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
---
+++ Mission08_Team0215/Migrations/20240302001827_TakeFour.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission08_Team0215.Migrations
+{
+    /// <inheritdoc />
+    public partial class TakeFour : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TaskName",
+                table: "UserTask",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TaskName",
+                table: "UserTask",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
